3599ca0e4b2d7562df3ba4dda45153b416b8e7a6
Dazzler <117281593+DDazzler@users.noreply.github.com>
2022-12-05 17:20:52 +0530

    Update nodeinstall.sh

--- nodeinstall.sh
+++ nodeinstall.sh
@@ -3,8 +3,7 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install 12
-sudo ln -s $(which node) /usr/bin/
-sudo ln -s $(which npm) /usr/bin/
+
 
 
 
